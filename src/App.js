ffd5ffadae263e386f972d5abb7afeacc21e71c5
shashank231 <65719041+shashank231@users.noreply.github.com>
2023-12-29 17:30:53 +0530

    Main logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Header from './components/Header';
+import Notes from './pages/Notes';
+import Note from './pages/Note';
 
 function App() {
+  let notespage = (
+    <React.Fragment>
+      <div className="container dark">
+        <div className="app">
+          <Header />
+          <Notes />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+
+  let singlenote = (
+    <React.Fragment>
+      <div className="container dark">
+        <div className="app">
+          <Note />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+
   return (
-    <div className="App">
-       My Notes
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/" element={notespage}/>
+          <Route path="/note/:id" element={singlenote} />
+      </Routes>
+    </Router>
   );
 }
 
